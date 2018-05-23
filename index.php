88aad1ead01bbd071ed595f769a6a32032dfbd85
iPisi <xpisi1999@gmail.com>
2018-05-23 22:09:46 +0200

    Aggiunta SASS

--- index.php
+++ index.php
@@ -2,8 +2,16 @@
 <html>
 <head>
 	<title>NULL *</title>
+	<link rel="shortcut icon" href="res/icon.png" />
+	<link rel="stylesheet" href="css/main.css">
 </head>
 <body>
 
+	<div class="navBar">
+		<img src="res/icon.png" class="logo">
+	</div>
+
+	<div class="content">
+	</div>
 </body>
 </html>
